f31807373488aac8cc3b8db579f4aa462f8a6285
Anthony Bill <36425922+abill1@users.noreply.github.com>
2024-04-13 18:38:36 -0500

    Add Engine header.

---
+++ Engine/Engine.h
@@ -0,0 +1,22 @@
+
+#ifndef ENGINE_H
+#define ENGINE_H
+
+//======================================================================
+// Vendor Includes
+//======================================================================
+
+#include "enTT/entt.hpp"
+#include "glad/glad.h"
+#include "SDL2/SDL.h"
+#include "glm/glm.hpp"
+
+//======================================================================
+// Engine Includes
+//======================================================================
+
+#include "Framework/MemoryTracker/MemoryTracker.h"
+
+#endif // !ENGINE_H
+
+
