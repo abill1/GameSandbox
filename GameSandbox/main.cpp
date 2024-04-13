a72ccaf7f7b4ef5bc2449431b8e16985039fdcf1
Anthony Bill <36425922+abill1@users.noreply.github.com>
2024-04-13 18:39:04 -0500

    Modified to use updated headers and test added code.

--- GameSandbox/main.cpp
+++ GameSandbox/main.cpp
@@ -1,21 +1,20 @@
 
-#include "enTT/entt.hpp"
-#include "glad/glad.h"
-#include "SDL2/SDL.h"
-#include "glm/glm.hpp"
+#include "./Engine/Engine.h"
 #include <stdio.h>
 
 struct OpenGLAttributes
 {
-	Uint8 Major;
-	Uint8 Minor;
-	Uint8 Profile;
-	Uint8 DoubleBuffer;
-	Uint8 DepthBufferSize;
+	Uint8 Major = 4;
+	Uint8 Minor = 0;
+	Uint8 Profile = 1;
+	Uint8 DoubleBuffer = 1;
+	Uint8 DepthBufferSize = 24;
+	Uint8 AntiAliasing = 4;
 };
 
 int main(int argc, char* argv[])
 {
+	Framework::MemoryTracker::Init();
 	SDL_Init(SDL_INIT_EVERYTHING);
 	entt::registry m_Registry;
 	entt::entity test0 = m_Registry.create();
@@ -25,19 +24,23 @@ int main(int argc, char* argv[])
 	SDL_DisplayMode displayMode;
 	SDL_GetCurrentDisplayMode(0, &displayMode);
 	SDL_Log("Resolution: %d x %d | Refresh Rate: %d | Pixel Format: %s\n", displayMode.w, displayMode.h, displayMode.refresh_rate, SDL_GetPixelFormatName(displayMode.format));
+	SDL_GetCurrentVideoDriver();
 
 	// ----- Create window and renderer 
-	OpenGLAttributes oglAttr = { 4, 0, 1, 1, 24 };
+	OpenGLAttributes oglAttr;
 	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, oglAttr.Major);
 	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, oglAttr.Minor);
 	SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, oglAttr.Profile);
 	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, oglAttr.DoubleBuffer);
 	SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, oglAttr.DepthBufferSize);
+	SDL_GL_SetAttribute(SDL_GL_MULTISAMPLESAMPLES, oglAttr.AntiAliasing);
 	SDL_Window* window = SDL_CreateWindow("SDL Test", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, displayMode.w / 2, displayMode.h / 2, SDL_WINDOW_SHOWN | SDL_WINDOW_OPENGL);
 	SDL_GLContext openGLContext = SDL_GL_CreateContext(window);
 	gladLoadGLLoader(SDL_GL_GetProcAddress);
+	
+	glm::vec4 clearColor = { 0.5f, 0.5f, 0.5f, 1.0f };
 	glViewport(SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, displayMode.w / 2, displayMode.h / 2);
-	glClearColor(0.5f, 0.5f, 0.5f, 1.0f);
+	glClearColor(clearColor.r, clearColor.g, clearColor.b, clearColor.a);
 
 	// ----- Game Loop
 	bool bRunning = true;
