bcf27ff940566acf57dda89a97d60be9753584c2
Anthony Bill <36425922+abill1@users.noreply.github.com>
2024-04-13 19:16:11 -0500

    Add file system.

---
+++ Engine/Framework/File/ABFile.cpp
@@ -0,0 +1,73 @@
+
+#include "ABFile.h"
+#include "../Codes/ABCode.h"
+//#include "../ABString/ABString.h"
+//#include "../Buffer/Buffer.h"
+#include <stdio.h>
+
+Framework::Code Framework::File::Load(char* buffer, size_t _bufSize, const char* _filePath)
+{
+	Framework::Code code = Framework::Code::FAILURE;
+
+	FILE* hFile;
+	errno_t err = fopen_s(&hFile, _filePath, "r+");
+	if (err == 0)
+	{
+		printf("File opened\n");
+		fseek(hFile, 0, SEEK_END);
+		long len = ftell(hFile);
+		rewind(hFile);
+		fread_s(buffer, _bufSize, sizeof(char), len - 1, hFile);
+		rewind(hFile);
+		fclose(hFile);
+		code = Framework::Code::SUCCESS;
+	}
+
+	return code;
+}
+
+/*
+Framework::Code Framework::File::Load(char* buffer, size_t _bufSize, const Framework::String& _filePath)
+{
+	Framework::Code code = Framework::Code::FAILURE;
+
+	FILE* hFile;
+	errno_t err = fopen_s(&hFile, _filePath.c_str(), "r+");
+	if (err == 0)
+	{
+		printf("File opened\n");
+		fseek(hFile, 0, SEEK_END);
+		long len = ftell(hFile);
+		rewind(hFile);
+		fread_s(buffer, _bufSize, sizeof(char), len - 1, hFile);
+		rewind(hFile);
+		fclose(hFile);
+		code = Framework::Code::SUCCESS;
+	}
+
+	return code;
+}
+*/
+
+/*
+Framework::Code Framework::File::Load(Framework::Buffer& _buffer, const Framework::String& _filePath)
+{
+	Framework::Code code = Framework::Code::FAILURE;
+
+	FILE* hFile;
+	errno_t err = fopen_s(&hFile, _filePath.c_str(), "r+");
+	if (err == 0)
+	{
+		printf("File opened\n");
+		fseek(hFile, 0, SEEK_END);
+		size_t len = (size_t)ftell(hFile);
+		rewind(hFile);
+		_buffer.Fill(hFile, len);
+		rewind(hFile);
+		fclose(hFile);
+		code = Framework::Code::SUCCESS;
+	}
+
+	return code;
+}
+*/
