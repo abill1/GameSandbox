84a0650228cf35f67e297edc4e553a2d1320262d
Anthony Bill <36425922+abill1@users.noreply.github.com>
2024-04-20 18:10:38 -0500

    Added framework file with includes of subfolders and macros.

---
+++ Engine/Framework/Framework.h
@@ -0,0 +1,34 @@
+
+#ifndef ENGINE_FRAMEWORK_H
+#define ENGINE_FRAMEWORK_H
+
+//======================================================================
+// Includes
+//======================================================================
+
+#include "Buffer/Buffer.h"
+#include "Codes/ABCode.h"
+#include "Constants/Constants.h"
+#include "File/ABFile.h"
+#include "MemoryTracker/MemoryTracker.h"
+#include "String/ABString.h"
+
+//======================================================================
+// Assert Macros
+//======================================================================
+
+#include <assert.h>
+
+#define CHECK_NULL(x) assert(x != nullptr)
+#define CHECK_SUCCESS(x) assert(x == Framework::Code::SUCCESS);
+#define CHECK_EQUAL(x, y) assert(x == y);
+
+//======================================================================
+// Helper Macros
+//======================================================================
+
+#define UNUSED_VAR(x) (void(x))
+
+
+#endif // !ENGINE_FRAMEWORK_H
+
