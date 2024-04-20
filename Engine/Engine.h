3bc07c103a4abc67d5a2c2d2d637539fbf3ef31a
Anthony Bill <36425922+abill1@users.noreply.github.com>
2024-04-20 18:13:05 -0500

    Updated to includes.

--- Engine/Engine.h
+++ Engine/Engine.h
@@ -15,12 +15,18 @@
 // Engine Includes
 //======================================================================
 
-#include "Framework/MemoryTracker/MemoryTracker.h"
-#include "Framework/Codes/ABCode.h"
-#include "Framework/File/ABFile.h"
+// ----- Framework Includes
 #include "Framework/Buffer/Buffer.h"
+#include "Framework/Codes/ABCode.h"
 #include "Framework/Constants/Constants.h"
+#include "Framework/File/ABFile.h"
+#include "Framework/MemoryTracker/MemoryTracker.h"
 #include "Framework/String/ABString.h"
+
+// ----- Renderer - OpenGL Includes
+#include "Renderer/Renderer.h"
+
+
 
 #endif // !ENGINE_H
 
